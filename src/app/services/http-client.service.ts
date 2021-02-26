9f388b02016eb1835992d8b5ff0374791df1d97d
wakkieeeeee <ttwh.iibnmk6699s@gmail.com>
2021-02-26 23:42:16 +0900

    commit

--- src/app/services/http-client.service.ts
+++ src/app/services/http-client.service.ts
@@ -19,7 +19,7 @@ export class HttpClientService {
 
   public get(): Promise<any[]> {
     // return this.http.get(this.host + '/get', this.httpOptions)
-    return this.http.get(this.host + '/api/v1')
+    return this.http.get(this.host + '/api/v1/message')
       .toPromise()
       .then((res) => {
         const response: any = res;
@@ -28,11 +28,42 @@ export class HttpClientService {
       .catch(this.errorHandler);
   }
 
+  //
+  public articleGet(): Promise<any[]> {
+    return this.http.get(this.host + '/api/v1/article')
+      .toPromise()
+      .then((res) => {
+        const response: any = res;
+        return response;
+      })
+      .catch(this.errorHandler);
+  }
+
+
   private errorHandler(err) {
     console.log('Error occurred.', err);
     return Promise.reject(err.message || err);
   }
 
+  public register(body: any): Promise<any[]> {
+    return this.http.post(this.host + '/api/v1/message/post', body)
+      .toPromise()
+      .then((res) => {
+        const response: any = res;
+        return response;
+      })
+      .catch(this.errorHandler);
+  }
 
+  public delete(body: any): Promise<any[]> {
+    console.log('httpClientService -> body: ', body)
+    return this.http.delete(this.host + '/api/v1/message/delete', body)
+      .toPromise()
+      .then(res => {
+        const response: any = res;
+        return response;
+      })
+      .catch(this.errorHandler);
+  }
 }
 
