c28f20a93913225b9c39cdbf68b795dd78ae8342
wakkieeeeee <ttwh.iibnmk6699s@gmail.com>
2021-03-06 00:36:16 +0900

    commit

--- src/app/components/temp-page/temp-page.component.ts
+++ src/app/components/temp-page/temp-page.component.ts
@@ -8,6 +8,8 @@ import { HttpClientService } from '../../services/http-client.service';
   templateUrl: './temp-page.component.html',
   styleUrls: ['./temp-page.component.css']
 })
+
+
 export class TempPageComponent implements OnInit {
 
   // private tempUrl = 'http://localhost:3000/api/v1/user/test';
@@ -16,10 +18,19 @@ export class TempPageComponent implements OnInit {
   public param: any = {};
   public articleParam: any = {};
 
+  // public messageInfo: any = {
+  //   id: null,
+  //   message: null
+  // };
+
   public messageInfo: any = {
-    id: null,
-    message: null
-  };
+    no: null,
+    message: null,
+    date: null,
+    isEdit: true
+  }
+
+
 
   public messageInfoList: any = [this.messageInfo];
   public articleList: any;
@@ -39,16 +50,29 @@ export class TempPageComponent implements OnInit {
     //   return value.toString();
     // }));
 
-    this.httpClientService.get()
-      .then(
-        (response) => {
-          this.param = response;
-          this.messageInfoList = this.param.messages;
-        }
-      )
-      .catch(
-        (error) => console.log(error)
-      );
+
+    // 以下をBackend APIから受け取る
+    //   status: 200,
+    //   response: 'Hello, this is message api',
+    //   messages: storages --> id, message
+    // this.httpClientService.get()
+    //   .then(
+    //     (response) => {
+    //       this.param = response;
+    //       this.messageInfoList = this.param.messages;
+
+    //       // this.messageInfoList
+    //       console.log('this.messageInfoList.length:', this.messageInfoList.length);
+    //       console.log('this.messageInfoList:', this.messageInfoList);
+
+    //       this.messageId = this.messageInfoList.length;
+    //     }
+    //   )
+    //   .catch(
+    //     (error) => console.log(error)
+    //   );
+
+    this.testGet();
 
     this.httpClientService.articleGet()
       .then((response) => {
@@ -58,20 +82,53 @@ export class TempPageComponent implements OnInit {
       })
   }
 
-  // getPage() {
-  //   return this.http.get(this.tempUrl);
-  // }
+  private testGet() {
+    this.httpClientService.testget()
+    .then(
+      (response) => {
+        this.param = response;
+        this.messageInfoList = this.param;
+        console.log('messageInfoList: ', this.messageInfoList);
+      }
+    )
+    .catch(
+      (error) => console.log(error)
+    );
+  }
 
   public onClickRegister() {
+      const body: any = {
+        id: this.messageId,
+        message: this.message
+      };
+      this.httpClientService.register(body)
+        .then(
+          (response) => {
+            this.param = response;
+            this.messageInfoList = this.param.messages;
+            this.messageId++;
+          }
+        )
+        .catch(
+          (error) => console.log(error)
+        );
+  }
+
+
+  // http clientでHTMLから受け取ったデータをバックエンドAPIに渡して、
+  // DBに格納、再表示
+  public onClickRegister2(message: string) {
     const body: any = {
-      id: this.messageId,
-      message: this.message
-    };
+      message: message
+    }
     this.httpClientService.register(body)
       .then(
         (response) => {
           this.param = response;
-          this.messageInfoList = this.param.messages;
+          console.log('param.message: ', this.param.message);
+          this.messageInfoList.message = this.param.message;
+          //自動で再表示されないので、以下を実行
+          this.testGet();
         }
       )
       .catch(
@@ -79,23 +136,43 @@ export class TempPageComponent implements OnInit {
       );
   }
 
-  public onClickDelete() {
-    console.log('messageId: ', this.messageId);
-    console.log('message: ', this.message);
-    const body: any = {
-      id: this.messageId,
-      message: this.message
-    };
 
+  public onClickDelete(no: number): void {
+    console.log('no: ', no);
+    const body: any = {
+      no: no,
+    };
+    console.log('body: ',body);
     this.httpClientService.delete(body)
     .then(
       response => {
         this.param = response;
         this.messageInfoList = this.param.messages;
+        console.log('messageInfoList: ', this.messageInfoList);
+        this.testGet();
       }
     )
     .catch(
       (error) => console.log(error)
     );
   }
+  // public onClickDelete() {
+  //   console.log('messageId: ', this.messageId);
+  //   console.log('message: ', this.message);
+  //   const body: any = {
+  //     id: this.messageId,
+  //     message: this.message
+  //   };
+  //   console.log('body: ',body);
+  //   this.httpClientService.delete(body)
+  //   .then(
+  //     response => {
+  //       this.param = response;
+  //       this.messageInfoList = this.param.messages;
+  //     }
+  //   )
+  //   .catch(
+  //     (error) => console.log(error)
+  //   );
+  // }
 }
--- src/app/services/http-client.service.ts
+++ src/app/services/http-client.service.ts
@@ -12,16 +12,41 @@ export class HttpClientService {
   // private host: string = 'http://localhost:4203/app'
   private host: string = 'http://localhost:3000'
 
+  // 認証トークンを使用するために以下は必要になるが、一旦使わない
+  private httpOptions: any = {
+    headers: new HttpHeaders({
+      // 'Contetnt-Type': 'application/json'
+      'Contetnt-Type': 'text/plain'
+    }),
+    // delete 実行時に'body'が必要になるケースがあるため、プロパティとして残しておく
+    body: null
+  }
+
 
   constructor(
     private http: HttpClient
   ) { }
 
+  // 以下をReturn
+  //   status: 200,
+  //   response: 'Hello, this is message api',
+  //   messages: storages --> id, message
   public get(): Promise<any[]> {
     // return this.http.get(this.host + '/get', this.httpOptions)
     return this.http.get(this.host + '/api/v1/message')
       .toPromise()
       .then((res) => {
+        const response: any = res;
+        return response;
+      })
+      .catch(this.errorHandler);
+    }
+
+    public testget(): Promise<any[]> {
+      return this.http.get(this.host + '/api/v1/message/testget')
+      .toPromise()
+      .then((res) => {
+        console.log('res: ', res);
         const response: any = res;
         return response;
       })
@@ -46,7 +71,7 @@ export class HttpClientService {
   }
 
   public register(body: any): Promise<any[]> {
-    return this.http.post(this.host + '/api/v1/message/post', body)
+    return this.http.post(this.host + '/api/v1/message', body)
       .toPromise()
       .then((res) => {
         const response: any = res;
@@ -56,8 +81,10 @@ export class HttpClientService {
   }
 
   public delete(body: any): Promise<any[]> {
-    console.log('httpClientService -> body: ', body)
-    return this.http.delete(this.host + '/api/v1/message/delete', body)
+    // this.httpOptions.body = body.no;
+    // console.log('httpClientService -> httpOptions.body: ', this.httpOptions)
+    // return this.http.delete(this.host + '/api/v1/message/delete', this.httpOptions)
+    return this.http.post(this.host + '/api/v1/message/delete', body)
       .toPromise()
       .then(res => {
         const response: any = res;
